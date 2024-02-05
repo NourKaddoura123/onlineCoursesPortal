5e46be99472725a3a296cc8cf4ba344c53c9fce3
ninnnn53535 <nhk21@mail.aub.edu>
2024-02-06 01:49:20 +0200

    added a sidenavbar

--- app/(router)/layout.js
+++ app/(router)/layout.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import SideNav from './_components/SideNav'
 
 function layout({children}) { //pages going to render go to this layout
   return (
-    <div>{children}</div>
+    <div>
+    <div className='sm:w-64 sm:block fixed'>
+        <SideNav/>
+        </div>
+        <div className='sml-64'>
+        {children}</div></div>
   )
 }
 
