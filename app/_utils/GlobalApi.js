08025efc17c3b5b4f9c0c808cbeba24108b224bb
ninnnn53535 <nhk21@mail.aub.edu>
2024-02-07 00:31:56 +0200

    connected hygraph, added dependency graphql and made a request

---
+++ app/_utils/GlobalApi.js
@@ -0,0 +1,34 @@
+const { gql } = require("graphql-request")
+
+const MASTER_URL="https://api-us-east-1-shared-usea1-02.hygraph.com/v2/"+process.env.NEXT_PUBLIC_HYGRAPH_API_KEY+"/master"
+
+//make request
+const getAllCourseList=async()=>{
+    const query=gql`
+    query Courses {
+        courseLists {
+          name
+          description
+          free
+          totalChapters
+          banner {
+            id
+          }
+          chapter {
+            ... on Chapter {
+              id
+              name
+              chapterNumber
+              youtubeUrl
+            }
+          }
+        }
+      }
+      `
+      const result=await request(MASTER_URL,query);
+      return result;
+}
+
+export default{
+    getAllCourseList
+}
